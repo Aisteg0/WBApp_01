ed3825406a43ab272e29a78c31a634398327c2b1
Misha <Mishka.ganin@mail.ru>
2024-06-05 14:50:44 +0300

    init

---
+++ WBApp/WBAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WBAppApp.swift
+//  WBApp
+//
+//  Created by Михаил Ганин on 05.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct WBAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
